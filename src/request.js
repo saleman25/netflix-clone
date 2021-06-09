eeb8bbcd74dff5e21a03f1a9ff4ca45145917edc
Samantha Aleman <samantha.aleman25@gmail.com>
2021-06-09 14:30:28 -0700

    fetch requests

---
+++ src/request.js
@@ -0,0 +1,14 @@
+const APIKEY = '0c6516a939311eb7559a5339e3009c30';
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key${APIKEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key${APIKEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key${APIKEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key${APIKEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key${APIKEY}&with_genres=99`,
+}
+
+export default requests;
