3508fadf9a31880aa755c0dbb3313a55a0bf9e30
Samantha Aleman <samantha.aleman25@gmail.com>
2021-06-09 16:11:22 -0700

    added styling to the rows

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
-import './Row.css';
-
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -21,12 +20,14 @@ function Row({ title, fetchUrl }) {
     <div className="row">
       <h2>{title}</h2>
 
-      <div className="row_posters">
+      <div className='row_posters'>
         {movies.map((movie) => (
           <img
             key={movie.id}
-            className="row_poster"
-            src={`${base_url}${movie.poster_path}`}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           ></img>
         ))}
