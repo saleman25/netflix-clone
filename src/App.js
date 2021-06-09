ee2934fb002fbc7e37709296a13678c29f0fd452
Samantha Aleman <samantha.aleman25@gmail.com>
2021-06-09 14:59:20 -0700

    row component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
+import requests from './requests'
 
 function App() {
   return (
     <div className="App">
       <h1>Hi front end :3</h1>
+      <Row title='Netflix Originals' fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+      
     </div>
   );
 }
