68bb90e2f901de0932a301763a3c6685db69d592
Samantha Aleman <samantha.aleman25@gmail.com>
2021-06-11 11:34:06 -0700

    finished nav styling

--- src/Nav.js
+++ src/Nav.js
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
 
 function Nav() {
-    return (
-        <div className='nav'>
-            <img className='nav_logo'
-            src='https://assets.brand.microsites.netflix.io/assets/493f5bba-81a4-11e9-bf79-066b49664af6_cm_1440w.png?v=8'
-            alt='Netflix Logo'
-            >
-            </img>
-            <img className='nav_avatar'
-            src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
-            alt='Netflix Logo'
-            >
-            </img>
-        </div>
-    )
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1597px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      ></img>
+      <img
+        className="nav_avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Logo"
+      ></img>
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
